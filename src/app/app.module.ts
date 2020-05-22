f8d1940fa10231464c55bbef7058d4d73c9f6107
mary <maradon853@gmail.com>
2020-05-22 13:43:05 -0500

    add routing, components, and book class

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { UserAccountComponent } from './user-account/user-account.component';
+import { HomepageComponent } from './homepage/homepage.component';
+import { BookListComponent } from './book-list/book-list.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    UserAccountComponent,
+    HomepageComponent,
+    BookListComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/books.ts
@@ -0,0 +1,13 @@
+export class Books {
+    title: string;
+    author: string;
+    genre: string;
+
+    constructor(title: string, author: string, genre: string){
+        this.title = title;
+        this.author = author;
+        this.genre = genre;
+    }
+
+    
+}
