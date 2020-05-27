16b9b51aa11192aab5af646549d9715357094618
mary <maradon853@gmail.com>
2020-05-27 14:51:07 -0500

    create myBooks.ts to store book titles

---
+++ src/app/myBooks.ts
@@ -0,0 +1,9 @@
+import { Book } from "./book";
+
+export const BOOKS: Book[] = [
+    new Book("Frankenstien", "Mary Shelly", "Science Fiction"),
+    new Book("Swamp Thing", "Allan Moore", "Graphic Novel")
+]
+
+
+
