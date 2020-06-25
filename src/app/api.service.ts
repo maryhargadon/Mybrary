9665ff66095bbc4cc45e8f8ebb303b669d291c2d
mary <maradon853@gmail.com>
2020-06-25 15:58:32 -0500

    changes api routes so that book funtions/nav work

--- src/app/api.service.ts
+++ src/app/api.service.ts
@@ -29,38 +29,38 @@ export class ApiService {
 //methods for lending books:
 
   getBooks(): Observable<LendingBook[]> {
-    return this.http.get<LendingBook[]>(apiUrl)
+    return this.http.get<LendingBook[]>('/api/get-books')
       .pipe(
         tap(books => console.log('fetched Books')),
         catchError(this.handleError('getBooks', []))
       );
   }
   getBook(id: number): Observable<LendingBook> {
-    const url = `${apiUrl}/${id}`;
-    return this.http.get<LendingBook>(url).pipe(
+    //const url = `${apiUrl}/${id}`;
+    return this.http.get<LendingBook>('/api/get-book/' + id).pipe(
       tap(_ => console.log(`fetched Book id=${id}`)),
       catchError(this.handleError<LendingBook>(`getBook id=${id}`))
     );
   }
 
   addLendingBook(book: LendingBook): Observable<LendingBook> {
-    return this.http.post<LendingBook>(apiUrl, book, httpOptions).pipe(
+    return this.http.post<LendingBook>('/api/add-book', book, httpOptions).pipe(
       tap((bk: LendingBook) => console.log(`added Book w/ id=${bk._id}`)),
       catchError(this.handleError<LendingBook>('addBook'))
     );
   }
 
   updateBook(id: any, book: LendingBook): Observable<any> {
-    const url = `${apiUrl}/${id}`;
-    return this.http.put(url, book, httpOptions).pipe(
+    //const url = `${apiUrl}/${id}`;
+    return this.http.put('/api/update-book/' + id, book, httpOptions).pipe(
       tap(_ => console.log(`updated Book id=${id}`)),
       catchError(this.handleError<any>('updateBook'))
     );
   }
 
   deleteBook(id: any): Observable<LendingBook> {
-    const url = `${apiUrl}/${id}`;
-    return this.http.delete<LendingBook>(url, httpOptions).pipe(
+    //const url = `${apiUrl}/${id}`;
+    return this.http.delete<LendingBook>('/api/delete-book/' + id, httpOptions).pipe(
       tap(_ => console.log(`deleted Book id=${id}`)),
       catchError(this.handleError<LendingBook>('deleteBook'))
     );
--- src/app/routes/lendingBook-route.ts
+++ src/app/routes/lendingBook-route.ts
@@ -4,37 +4,43 @@ import { LendingBooks } from '../models/lendingBook';
 export class LendingBookRoute {
 
     public lendingBookRoute(app): void {
-      app.route('/api/').get((req: Request, res: Response, next: NextFunction) => {
-        LendingBooks.find((err, lendingBooks) => {
-          if (err) { return next(err); }
-          res.json(lendingBooks);
-        });
-      });
-  
-      app.route('/api/:id').get((req: Request, res: Response, next: NextFunction) => {
+
+      //get single book
+      app.route('/api/get-book/:id').get((req: Request, res: Response, next: NextFunction) => {
         LendingBooks.findById(req.params.id, (err, lendingBooks) => {
           if (err) { return next(err); }
           res.json(lendingBooks);
         });
       });
-  
-      app.route('/api/').post((req: Request, res: Response, next: NextFunction) => {
-        console.log(req.body);
-        LendingBooks.create(req.body, (err, lendingBooks) => {
-          if (err) { return next(err); }
-          res.json(lendingBooks);
-        });
-      });
-  
-      app.route('/api/:id').put((req: Request, res: Response, next: NextFunction) => {
+      
+      //update book
+      app.route('/api/update-book/:id').put((req: Request, res: Response, next: NextFunction) => {
         LendingBooks.findByIdAndUpdate(req.params.id, req.body, (err, lendingBooks) => {
           if (err) { return next(err); }
           res.json(lendingBooks);
         });
       });
-  
-      app.route('/api/:id').delete((req: Request, res: Response, next: NextFunction) => {
+      
+      //delete book
+      app.route('/api/delete-book/:id').delete((req: Request, res: Response, next: NextFunction) => {
         LendingBooks.findByIdAndRemove(req.params.id, req.body, (err, lendingBooks) => {
+          if (err) { return next(err); }
+          res.json(lendingBooks);
+        });
+      });
+
+      //get all books
+      app.route('/api/get-books').get((req: Request, res: Response, next: NextFunction) => {
+        LendingBooks.find((err, lendingBooks) => {
+          if (err) { return next(err); }
+          res.json(lendingBooks);
+        });
+      });
+
+      //add (create) book:
+      app.route('/api/add-book').post((req: Request, res: Response, next: NextFunction) => {
+        console.log(req.body);
+        LendingBooks.create(req.body, (err, lendingBooks) => {
           if (err) { return next(err); }
           res.json(lendingBooks);
         });
