64f244267c27cb02a2923a53a03f7cb103913930
mary <maradon853@gmail.com>
2020-06-27 20:00:06 -0500

    fixed albums

--- src/app/routes/user-route.ts
+++ src/app/routes/user-route.ts
@@ -4,21 +4,21 @@ import { Users } from '../models/user';
 export class UserRoute {
 
     public userRoute(app): void {
-      app.route('/api/').get((req: Request, res: Response, next: NextFunction) => {
+      app.route('/api/get-users').get((req: Request, res: Response, next: NextFunction) => {
         Users.find((err, users) => {
           if (err) { return next(err); }
           res.json(users);
         });
       });
   
-      app.route('/api/:id').get((req: Request, res: Response, next: NextFunction) => {
+      app.route('/api/get-user/:id').get((req: Request, res: Response, next: NextFunction) => {
         Users.findById(req.params.id, (err, user) => {
           if (err) { return next(err); }
           res.json(user);
         });
       });
   
-      app.route('/api/').post((req: Request, res: Response, next: NextFunction) => {
+      app.route('/api/add-user').post((req: Request, res: Response, next: NextFunction) => {
         console.log(req.body);
         Users.create(req.body, (err, user) => {
           if (err) { return next(err); }
@@ -26,14 +26,14 @@ export class UserRoute {
         });
       });
   
-      app.route('/api/:id').put((req: Request, res: Response, next: NextFunction) => {
+      app.route('/api/update-user/:id').put((req: Request, res: Response, next: NextFunction) => {
         Users.findByIdAndUpdate(req.params.id, req.body, (err, user) => {
           if (err) { return next(err); }
           res.json(user);
         });
       });
   
-      app.route('/api/:id').delete((req: Request, res: Response, next: NextFunction) => {
+      app.route('/api/delete-user/:id').delete((req: Request, res: Response, next: NextFunction) => {
         Users.findByIdAndRemove(req.params.id, req.body, (err, user) => {
           if (err) { return next(err); }
           res.json(user);
