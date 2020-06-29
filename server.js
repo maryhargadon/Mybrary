b5b8f80c4dc12613d5c8a92a9abb9e008a3b154c
mary <maradon853@gmail.com>
2020-06-29 12:24:17 -0500

    adding files for user api

--- server.js
+++ server.js
@@ -1,3 +1,5 @@
+require('rootpath')();
+
 const express = require('express');
 const mongoose = require('mongoose');
 const config = require('config');
@@ -5,7 +7,32 @@ const app = express();
 
 const db = config.get('mongoURI');
 
+const cors = require('cors');
+const bodyParser = require('body-parser');
+const jwt = require('_helpers/jwt');
+const errorHandler = require('_helpers/error-handler');
+
 mongoose
   .connect(db, { useNewUrlParser: true, useCreateIndex: true, useFindAndModify: false, useUnifiedTopology: true })
   .then(() => console.log('MongoDB Connected...'))
   .catch(err => console.log(err));
+
+
+  app.use(bodyParser.urlencoded({ extended: false }));
+  app.use(bodyParser.json());
+  app.use(cors());
+  
+  // use JWT auth to secure the api
+  app.use(jwt());
+  
+  // api routes
+  app.use('/users', require('./users/users.controller'));
+  
+  // global error handler
+  app.use(errorHandler);
+  
+  // start server
+  const port = process.env.NODE_ENV === 'production' ? (process.env.PORT || 80) : 4000;
+  const server = app.listen(port, function () {
+      console.log('Server listening on port ' + port);
+  });  
