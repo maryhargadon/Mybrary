180457707bf4b3659e8dbe95db7b7dc05ac715f8
mary <maradon853@gmail.com>
2020-07-01 13:44:35 -0500

    changes schema

--- src/app/models/user.ts
+++ src/app/models/user.ts
@@ -1,12 +1,12 @@
 import mongoose, { Schema } from 'mongoose';
 
 const UserSchema: Schema = new Schema({
-    id: { type: String, required: true },
+    // id: { type: String, required: true },
     username: { type: String, required: true},
     password: { type: String, required: true},
     firstName: { type: String, required: true},
     lastName: { type: String, required: true},
-    token: { type: String, required: true}
+    //token: { type: String, required: true}
 });
 
 export const Users = mongoose.model('users', UserSchema);
