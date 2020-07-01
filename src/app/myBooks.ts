6fc32d9223a27548d28ef771bcd3c6eff4028e61
mary <maradon853@gmail.com>
2020-07-01 00:16:43 -0500

    changed book class to blank item

--- src/app/myBooks.ts
+++ src/app/myBooks.ts
@@ -1,8 +1,7 @@
 import { Book } from "./book";
 
 export const BOOKS: Book[] = [
-    new Book("Frankenstien", "Mary Shelly", "Science Fiction"),
-    new Book("Swamp Thing", "Allan Moore", "Graphic Novel")
+    
 ]
 
 
