7b61cdb8c233672574cc5c6c7a6aa1cd1db57747
mary <maradon853@gmail.com>
2020-07-13 12:42:26 -0500

    making changes to connect to api

--- server.js
+++ server.js
@@ -1,38 +1,37 @@
-//require('rootpath')();
+require('rootpath')();
 
 const express = require('express');
-const mongoose = require('mongoose');
-const config = require('config');
 const app = express();
-
-const db = config.get('mongoURI');
-
-//const cors = require('cors');
+const cors = require('cors');
 const bodyParser = require('body-parser');
-//const jwt = require('_helpers/jwt');
-//const errorHandler = require('_helpers/error-handler');
+const jwt = require('_helpers/jwt');
+const errorHandler = require('_helpers/error-handler');
 
-mongoose
-  .connect(db, { useNewUrlParser: true, useCreateIndex: true, useFindAndModify: false, useUnifiedTopology: true })
-  .then(() => console.log('MongoDB Connected...'))
-  .catch(err => console.log(err));
+// const mongoose = require('mongoose');
+// const config = require('config');
+// const db = config.get('mongoURI');
+
+// mongoose
+//   .connect(db, { useNewUrlParser: true, useCreateIndex: true, useFindAndModify: false, useUnifiedTopology: true })
+//   .then(() => console.log('MongoDB Connected...'))
+//   .catch(err => console.log(err));
 
 
   app.use(bodyParser.urlencoded({ extended: false }));
   app.use(bodyParser.json());
-  //app.use(cors());
+  app.use(cors());
   
   // use JWT auth to secure the api
-  //app.use(jwt());
+  app.use(jwt());
   
   // api routes
-  //app.use('/users', require('./users/users.controller'));
+  app.use('/users', require('./users/users.controller'));
   
   // global error handler
-  //app.use(errorHandler);
+  app.use(errorHandler);
   
   // start server
-  // const port = process.env.NODE_ENV === 'production' ? (process.env.PORT || 80) : 4000;
-  // const server = app.listen(port, function () {
-  //     console.log('Server listening on port ' + port);
-  // });  
+  const port = process.env.NODE_ENV === 'production' ? (process.env.PORT || 80) : 4000;
+  const server = app.listen(port, function () {
+      console.log('Server listening on port ' + port);
+  });  
