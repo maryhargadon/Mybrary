1d673344c1ccbb2af7d0e2045897363b575f5990
mary <maradon853@gmail.com>
2020-07-26 13:42:14 -0500

    cleaning up code

--- connect.js
+++ connect.js
@@ -1,6 +1,6 @@
 
 const MongoClient = require('mongodb').MongoClient;
-const uri = "mongodb+srv://maradon:Pamela1450!@cluster0-ww5ee.mongodb.net/mybrary?retryWrites=true&w=majority";
+const uri = "<mongoDB connection string>";
 const client = new MongoClient(uri, { useNewUrlParser: true });
 client.connect(err => {
   const collection = client.db("test").collection("devices");
