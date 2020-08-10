5851205f19cde7f1a3b8742fd798e270ad58b2f2
mary <maradon853@gmail.com>
2020-08-10 17:00:45 -0500

    adjusting admin routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -14,9 +14,11 @@ import { AddAlbumComponent } from './add-album/add-album.component';
 import { EditAlbumComponent } from './edit-album/edit-album.component';
 
 import { HomeComponent } from './home/home.component';
+import { AdminComponent } from './admin/admin.component';
 import { LoginComponent } from './login/login.component';
 import { RegisterComponent } from './register/register.component';
 import { AuthGuard } from './_helpers/auth.guard';
+import { Role } from './_models/role';
 
 const routes: Routes = [
 
@@ -37,6 +39,7 @@ const routes: Routes = [
   // { path: '', redirectTo: '/albums', pathMatch: 'full' },
 
   { path: '', component: HomeComponent, canActivate: [AuthGuard] }, 
+  { path: 'admin', component: AdminComponent, canActivate: [AuthGuard], data: { roles: [Role.Admin] } },
   { path: 'login', component: LoginComponent },
   { path: 'register', component: RegisterComponent },
   // otherwise redirect to home
